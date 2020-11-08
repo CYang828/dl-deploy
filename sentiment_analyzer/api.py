fbd8b91ad2db40673d6422d28c30f424a07a2d74
zhangchunyang <zhangchunyang_pri@126.com>
2020-11-08 13:08:07 +0800

    init

---
+++ sentiment_analyzer/api.py
@@ -0,0 +1,26 @@
+from typing import Dict
+
+from .classifier.model import Model, get_model
+
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class SentimentRequest(BaseModel):
+    text: str
+
+
+class SentimentResponse(BaseModel):
+    probabilities: Dict[str, float]
+    sentiment: str
+    confidence: float
+
+
+@app.post("/predict", response_model=SentimentResponse)
+def predict(request: SentimentRequest, model: Model = Depends(get_model)):
+    sentiment, confidence, probabilities = model.predict(request.text)
+    return SentimentResponse(
+        sentiment=sentiment, confidence=confidence, probabilities=probabilities
+    )
